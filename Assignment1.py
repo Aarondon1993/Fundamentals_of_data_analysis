1669b7e4507cbe023382417efd161346deb3ac46
Aarondon1993 <aarondonnelly@live.ie>
2020-10-17 15:16:28 +0100

    Added if condition

--- Assignment1.py
+++ Assignment1.py
@@ -1,3 +1,8 @@
 # Assignment 1 Fundamentals of data analysis
 
 # The first thing to do is request that the input from the user is given in list format
+print ('This program will identify the type of data the user inputs and then calculate the amount of repeats')
+x= input('Please enter a list of elements seperated by a space    ')
+
+if type(x)==str:
+    print('Its a string')
