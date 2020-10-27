812e508fad4bb52b44f044cbbb6426b5fc727a9e
Aarondon1993 <aarondonnelly@live.ie>
2020-10-27 12:33:28 +0000

    Corrected program to a function and used *args as input from user

--- Assignment1.py
+++ Assignment1.py
@@ -1,8 +1,21 @@
-# Assignment 1 Fundamentals of data analysis
 
-# The first thing to do is request that the input from the user is given in list format
-print ('This program will identify the type of data the user inputs and then calculate the amount of repeats')
-x= input('Please enter a list of elements seperated by a space    ')
 
-if type(x)==str:
-    print('Its a string')
+def counts(*args):
+    freq= {}
+    for item in args:
+        if (item in freq):
+            freq [item] += 1
+        else:
+            freq [item] = 1
+
+    for key, value in freq.items():
+        print (key, ":" ,value)
+        
+
+
+counts(1,2,1,2,1,2,1,2,'a','a','e','e','f','f','iou','iou')                   
+
+    
+    
+        
+    
